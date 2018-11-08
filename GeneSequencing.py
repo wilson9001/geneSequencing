562b1bc4f27a809823e4b873dd427f87e57c388a
Alexander Wilson <wilsonalexanderjohn@byu.net>
2018-11-08 13:44:26 -0700

    Finished non-amortized section.

--- GeneSequencing.py
+++ GeneSequencing.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from enum import Enum, auto
+from enum import Enum, auto, IntEnum
 
 from which_pyqt import PYQT_VER
 if PYQT_VER == 'PYQT5':
@@ -38,105 +38,126 @@ class GeneSequencing:
 
 	def align( self, sequences, table, banded, align_length):
 		self.banded = banded
-		self.MaxCharactersToAlign = align_length
+		self.MaxCharactersToAlign = align_length+1
 		results = []
+		previous = 'previous'
+		cost = 'cost'
+		topStringAlignment = []
+		sideStringAlignment = []
 
+		maxArray = [[{previous: None, cost: math.inf} for x in range(self.MaxCharactersToAlign)] for y in range(self.MaxCharactersToAlign)]
+		
 		for row in range(len(sequences)):
 			rowResults = []
 			for column in range(len(sequences)):
 
 				if(column < row):
 					s = {}
+				elif column == row:
+					s = {'align_cost': -3 * (len(sequences[row]) if len(sequences[row]) < self.MaxCharactersToAlign else self.MaxCharactersToAlign-1), 'seqi_first100': sequences[row][:100], 'seqj_first100': sequences[row][:100]}
+					table.item(row, column).setText('{}'.format(s['align_cost']))
+					table.repaint()
 				else:
 ###################################################################################################
-# your code should replace these three statements and populate the three variables: score, alignment1 and alignment2
 					# extract sequences for this comparison
 					topString = sequences[row]
 					sideString = sequences[column]
 
-					# prepend dashes to each string
-					topStringDashed = "-" + topString
-					sideStringDashed = "-" + sideString
-
 					# set up matrix
-					topStringDashedLen = len(topStringDashed)
-					sideStringDashedLen = len(sideStringDashed)
+					topStringDashedLen = len(topString)+1 if len(topString)+1 <= self.MaxCharactersToAlign else self.MaxCharactersToAlign
+					sideStringDashedLen = len(sideString)+1 if len(sideString)+1 <= self.MaxCharactersToAlign else self.MaxCharactersToAlign
 
-					costMatrix = [[math.inf for x in range(topStringDashedLen)] for y in range(sideStringDashedLen)]
+					# set up strings
+					topStringDashed = "-" + topString[:topStringDashedLen-1]
+					sideStringDashed = "-" + sideString[:sideStringDashedLen-1]
+
+					costMatrix = None
+
+					if topStringDashedLen == self.MaxCharactersToAlign and sideStringDashedLen == self.MaxCharactersToAlign:
+						costMatrix = maxArray
+					else:
+						costMatrix = [[{previous: None, cost: math.inf} for x in range(topStringDashedLen)] for y in range(sideStringDashedLen)]
 
 					# initialize matrix
-					costMatrix[0][0] = {'previous': None, 'cost': 0}
+					costMatrix[0][0][cost] = 0
 
 					for innerColumn in range(1, topStringDashedLen):
-						costMatrix[0][innerColumn] = {'previous': Side.l, 'cost': costMatrix[0][innerColumn-1] + INDEL}
-
+						costMatrix[0][innerColumn][previous] = Side.l
+						costMatrix[0][innerColumn][cost] = costMatrix[0][innerColumn-1][cost] + INDEL
+						
 					for innerRow in range(1, sideStringDashedLen):
-						costMatrix[innerRow][0] = {'previous': Side.t, 'cost': costMatrix[innerRow-1][0] + INDEL}
-
+						costMatrix[innerRow][0][previous] = Side.t
+						costMatrix[innerRow][0][cost] = costMatrix[innerRow-1][0][cost] + INDEL
+						
 					for innerRow in range(1, sideStringDashedLen):
 						for innerColumn in range(1, topStringDashedLen):
 
-							bestOption = {}
+							bestOption = {previous: None, cost: None}
 
-							left = costMatrix[innerRow][innerColumn-1] + INDEL
-							diagonal = costMatrix[innerRow - 1][innerColumn - 1] + SUB
+							left = costMatrix[innerRow][innerColumn-1][cost] + INDEL
 
-							if topString[innerColumn] == sideString[row]:
-								diagonal = costMatrix[innerRow-1][innerColumn-1] + MATCH
+							diagonal = None
 
-							top = costMatrix[innerRow-1][innerColumn] + INDEL
+							if topStringDashed[innerColumn] == sideStringDashed[innerRow]:
+								diagonal = costMatrix[innerRow-1][innerColumn-1][cost] + MATCH
+							else:
+								diagonal = costMatrix[innerRow - 1][innerColumn - 1][cost] + SUB
+
+							top = costMatrix[innerRow-1][innerColumn][cost] + INDEL
 
 							if left < diagonal:
-								bestOption.previous = Side.l
-								bestOption.cost = left
+								bestOption[previous] = Side.l
+								bestOption[cost] = left
+								
 							else:
-								bestOption.previous = Side.d
-								bestOption.cost = diagonal
-
+								bestOption[previous] = Side.d
+								bestOption[cost] = diagonal
+								
 							if top < diagonal:
-								bestOption.previous = Side.t
-								bestOption.cost = top
+								bestOption[previous] = Side.t
+								bestOption[cost] = top
+								
+							costMatrix[innerRow][innerColumn][previous] = bestOption[previous]
+							costMatrix[innerRow][innerColumn][cost] = bestOption[cost]
+							
+					sideStringIndex = sideStringDashedLen-1
+					topStringIndex = topStringDashedLen-1
+					currentEntry = costMatrix[sideStringIndex][topStringIndex]
 
-							costMatrix[innerRow][innerColumn] = dict(bestOption)
+					score = currentEntry[cost]
 
-					currentEntryRow = sideStringDashedLen-1
-					currentEntryColumn = topStringDashedLen-1
-					currentEntry = costMatrix[currentEntryRow][currentEntryColumn]
+					topStringAlignment.clear()
+					sideStringAlignment.clear()
 
-					score = currentEntry.cost
-
-					topStringAlignment = []
-					sideStringAlignment = []
-
-					while currentEntry.previous is not None:
-						if currentEntry.previous == Side.l:
+					while currentEntry[previous] is not None:
+						if currentEntry[previous] == Side.l:
 							sideStringAlignment.append("-")
-							topStringAlignment.append(topStringDashed[currentEntryColumn])
-							currentEntryColumn -= 1
-						elif currentEntry.previous == Side.d:
-							sideStringAlignment.append(sideStringDashed[currentEntryRow])
-							topStringAlignment.append(topStringDashed[currentEntryColumn])
-							currentEntryRow -= 1
-							currentEntryColumn -= 1
+							topStringAlignment.append(topStringDashed[topStringIndex])
+							topStringIndex -= 1
+						elif currentEntry[previous] == Side.d:
+							sideStringAlignment.append(sideStringDashed[sideStringIndex])
+							topStringAlignment.append(topStringDashed[topStringIndex])
+							sideStringIndex -= 1
+							topStringIndex -= 1
 						else:
-							sideStringAlignment.append(sideStringDashed[currentEntryRow])
+							sideStringAlignment.append(sideStringDashed[sideStringIndex])
 							topStringAlignment.append("-")
-							currentEntryRow -= 1
+							sideStringIndex -= 1
 
-						currentEntry = costMatrix[currentEntryRow][currentEntryColumn]
-
+						currentEntry = costMatrix[sideStringIndex][topStringIndex]
+						
 					topStringAlignment.reverse()
 					sideStringAlignment.reverse()
 
+					topStringAlignmentFinal = ''.join(topStringAlignment)
+					sideStringAlignmentFinal = ''.join(sideStringAlignment)
+
 					# TODO: Create section for amortized later. Don't forget to set score again in new section
 
-					alignment1 = '{}  DEBUG:(seq{}, {} chars,align_len={}{})'.format(''.join(topStringAlignment), row+1, len(sequences[row]), align_length, ',BANDED' if banded else '')
-					alignment2 = '{}  DEBUG:(seq{}, {} chars,align_len={}{})'.format(''.join(sideStringAlignment), column+1, len(sequences[column]), align_length, ',BANDED' if banded else '')
+					alignment1 = '{}  DEBUG:(seq{}, {} chars,align_len={}{})'.format(topStringAlignmentFinal, row+1, len(sequences[row]), align_length, ',BANDED' if banded else '')
+					alignment2 = '{}  DEBUG:(seq{}, {} chars,align_len={}{})'.format(sideStringAlignmentFinal, column+1, len(sequences[column]), align_length, ',BANDED' if banded else '')
 
-					# score = row+column
-					# alignment1 = 'abc-easy  DEBUG:(seq{}, {} chars,align_len={}{})'.format(row+1, len(sequences[row]), align_length, ',BANDED' if banded else '')
-					# alignment2 = 'as-123--  DEBUG:(seq{}, {} chars,align_len={}{})'.format(column+1, len(sequences[column]), align_length, ',BANDED' if banded else '')
-###################################################################################################					
+###################################################################################################
 					s = {'align_cost':score, 'seqi_first100':alignment1, 'seqj_first100':alignment2}
 					table.item(row,column).setText('{}'.format(int(score) if score != math.inf else score))
 					table.repaint()
