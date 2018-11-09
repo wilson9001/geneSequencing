ccc2b709dd65784248ba45a61e0de8ca230faa48
Alexander Wilson <wilsonalexanderjohn@byu.net>
2018-11-08 21:16:00 -0700

    Created banded section but has scoring error.

--- GeneSequencing.py
+++ GeneSequencing.py
@@ -14,6 +14,7 @@ import time
 
 # Used to compute the bandwidth for banded version
 MAXINDELS = 3
+bandWidth = (2 * MAXINDELS) + 1
 
 # Used to implement Needleman-Wunsch scoring
 MATCH = -3
@@ -46,7 +47,8 @@ class GeneSequencing:
 		sideStringAlignment = []
 
 		maxArray = [[{previous: None, cost: math.inf} for x in range(self.MaxCharactersToAlign)] for y in range(self.MaxCharactersToAlign)]
-		
+		maxArray1 = [[{previous: None, cost: math.inf} for x in range(bandWidth)] for y in range(self.MaxCharactersToAlign)]
+
 		for row in range(len(sequences)):
 			rowResults = []
 			for column in range(len(sequences)):
@@ -62,6 +64,10 @@ class GeneSequencing:
 					# extract sequences for this comparison
 					topString = sequences[row]
 					sideString = sequences[column]
+
+					topStringAlignmentFinal = None
+					sideStringAlignmentFinal = None
+					score = None
 
 					if not self.banded:
 						# set up matrix
@@ -155,10 +161,152 @@ class GeneSequencing:
 
 						# TODO: Create section for amortized later. Don't forget to set score again in new section
 					else:
-						print("Banded")
-						topStringAlignmentFinal = ""
-						sideStringAlignmentFinal = ""
-						score = 0
+						topStringDashedLen = len(topString) + 1 if len(topString) + 1 <= self.MaxCharactersToAlign else self.MaxCharactersToAlign
+						sideStringDashedLen = len(sideString) + 1 if len(sideString) + 1 <= self.MaxCharactersToAlign else self.MaxCharactersToAlign
+
+						diff = topStringDashedLen - sideStringDashedLen
+
+						if abs(diff) > MAXINDELS:
+							score = math.inf
+							topStringAlignmentFinal = "No Alignment Possible"
+							sideStringAlignmentFinal = "No Alignment Possible"
+						else:
+							# set up strings
+							topStringDashed = "-" + topString[:topStringDashedLen - 1]
+							sideStringDashed = "-" + sideString[:sideStringDashedLen - 1]
+
+							costMatrix = None
+
+							# create matrix
+							if topStringDashedLen == self.MaxCharactersToAlign and sideStringDashedLen == self.MaxCharactersToAlign:
+								costMatrix = maxArray1
+							else:
+								costMatrix = [[{previous: None, cost: math.inf} for x in range(bandWidth)] for y in range(sideStringDashedLen)]
+
+							# fill in top-left corner
+							nills = MAXINDELS
+							innerRow = 0
+
+							while nills > 0:
+								for innerColumn in range(nills):
+									costMatrix[innerRow][innerColumn] = None
+								innerRow += 1
+								nills -= 1
+
+							# fill in bottom-right corner:
+							innerRow = sideStringDashedLen-1
+							nills = MAXINDELS-diff
+
+							while nills > 0:
+								for innerColumn in range(bandWidth-1, (bandWidth-1)-nills, -1):
+									costMatrix[innerRow][innerColumn] = None
+								innerRow -= 1
+								nills -= 1
+
+							startingColumn = MAXINDELS
+
+							# initialize matrix
+							costMatrix[0][startingColumn][cost] = 0
+							for innerColumn in range(startingColumn+1, bandWidth):
+								costMatrix[innerRow][innerColumn][cost] = costMatrix[innerRow][innerColumn-1][cost] + INDEL
+								costMatrix[innerRow][innerColumn][previous] = Side.l
+
+							startingColumn -= 1
+							topStringMinIndex = 1-MAXINDELS
+
+							for innerRow in range(1, sideStringDashedLen):
+								for innerColumn in range(startingColumn, bandWidth):
+
+									if costMatrix[innerRow][innerColumn] is not None:
+										bestOption = {previous: None, cost: None}
+
+										left = None
+
+										if innerColumn-1 < 0 or costMatrix[innerRow][innerColumn-1] is None:
+											left = math.inf
+										else:
+											left = costMatrix[innerRow][innerColumn - 1][cost] + INDEL
+
+										diagonal = None
+										topStringIndex = topStringMinIndex
+										topStringIndex += innerColumn
+
+										if costMatrix[innerRow - 1][innerColumn] is None:
+											diagonal = math.inf
+										elif topStringDashed[topStringMinIndex] == sideStringDashed[innerRow]:
+											diagonal = costMatrix[innerRow - 1][innerColumn][cost] + MATCH
+										else:
+											diagonal = costMatrix[innerRow - 1][innerColumn][cost] + SUB
+
+										top = None
+
+										if innerColumn+1 >= bandWidth:
+											top = math.inf
+										else:
+											top = costMatrix[innerRow - 1][innerColumn + 1][cost] + INDEL
+
+										# determine best option
+										if left < diagonal:
+											bestOption[previous] = Side.l
+											bestOption[cost] = left
+
+										else:
+											bestOption[previous] = Side.d
+											bestOption[cost] = diagonal
+
+										if top < diagonal:
+											bestOption[previous] = Side.t
+											bestOption[cost] = top
+
+										costMatrix[innerRow][innerColumn][previous] = bestOption[previous]
+										costMatrix[innerRow][innerColumn][cost] = bestOption[cost]
+
+								topStringMinIndex += 1
+								startingColumn = startingColumn - 1 if startingColumn > 0 else 0
+
+							sideStringIndex = sideStringDashedLen - 1
+							topStringIndex = topStringDashedLen - 1
+							#currentEntry = costMatrix[sideStringIndex][topStringIndex]
+							innerColumn = bandWidth-1
+							innerRow = sideStringIndex
+
+							currentEntry = costMatrix[innerRow][innerColumn]
+
+							while currentEntry is None:
+								innerColumn -= 1
+								currentEntry = costMatrix[innerRow][innerColumn]
+
+							score = currentEntry[cost]
+
+							topStringAlignment.clear()
+							sideStringAlignment.clear()
+###
+							while currentEntry[previous] is not None:
+								if currentEntry[previous] == Side.l:
+									sideStringAlignment.append("-")
+									topStringAlignment.append(topStringDashed[topStringIndex])
+									topStringIndex -= 1
+									innerColumn -= 1
+								elif currentEntry[previous] == Side.d:
+									sideStringAlignment.append(sideStringDashed[sideStringIndex])
+									topStringAlignment.append(topStringDashed[topStringIndex])
+									sideStringIndex -= 1
+									topStringIndex -= 1
+									innerRow -= 1
+								else:
+									sideStringAlignment.append(sideStringDashed[sideStringIndex])
+									topStringAlignment.append("-")
+									sideStringIndex -= 1
+									innerRow -= 1
+									innerColumn += 1
+
+								currentEntry = costMatrix[innerRow][innerColumn]
+
+							topStringAlignment.reverse()
+							sideStringAlignment.reverse()
+
+							topStringAlignmentFinal = ''.join(topStringAlignment)
+							sideStringAlignmentFinal = ''.join(sideStringAlignment)
 
 					alignment1 = '{}  DEBUG:(seq{}, {} chars,align_len={}{})'.format(topStringAlignmentFinal, row+1, len(sequences[row]), align_length, ',BANDED' if banded else '')
 					alignment2 = '{}  DEBUG:(seq{}, {} chars,align_len={}{})'.format(sideStringAlignmentFinal, column+1, len(sequences[column]), align_length, ',BANDED' if banded else '')
